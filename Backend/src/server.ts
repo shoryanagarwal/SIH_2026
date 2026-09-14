03ad9adc233d66275d4b69f5ee185c4d4d96f83c
Snehil Kumar <333snehilkumar7@gmail.com>
2026-09-14 17:05:36 +0530

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -11,7 +11,7 @@ import { pingMlService } from "./services/ml.service.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors());
 app.use(express.json());
